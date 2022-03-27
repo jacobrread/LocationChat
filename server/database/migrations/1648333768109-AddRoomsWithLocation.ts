e57bede4e96ee6284a7e82525159f70e06328cd7
jacobread <jread8888@gmail.com>
2022-03-26 18:56:04 -0600

    Did some of the back end but not all

---
+++ server/database/migrations/1648333768109-AddRoomsWithLocation.ts
@@ -0,0 +1,40 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export class AddRoomsWithLocation1648333768109 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'chat_room',
+        columns: [
+          {
+            name: 'id',
+            type: 'int',
+            isPrimary: true,
+            isGenerated: true,
+          },
+          {
+            name: 'name',
+            type: 'text',
+          },
+          {
+            name: 'roomkey',
+            type: 'text',
+            isUnique: true,
+          },
+          {
+            name: 'latitude',
+            type: 'int',
+          },
+          {
+            name: 'longitude',
+            type: 'int',
+          },
+        ],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('chat_room');
+  }
+}
